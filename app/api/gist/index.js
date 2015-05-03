fe5ae8557518322ff6a63b6a2bff93306d53896e
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2015-05-03 14:21:27 -0230

    Fix chaining indentation

--- app/api/gist/index.js
+++ app/api/gist/index.js
@@ -25,43 +25,40 @@ Gist.prototype._download = function _download() {
 Gist.prototype._master = function _master() {
 	var self = this;
 	return this._download()
-	.catch(function openExistingRepo() {
-		return NodeGit.Repository.open(self._path);
-	})
-	.then(function getMasterCommit(repo) {
-		return repo.getReferenceCommit("master");
-	});
+		.catch(function openExistingRepo() {
+			return NodeGit.Repository.open(self._path);
+		})
+		.then(function getMasterCommit(repo) {
+			return repo.getReferenceCommit("master");
+		});
 };
 
 Gist.prototype._getBlobs = function getBlobs(commit) {
 	return commit.getTree()
-	.then(function (tree) {
-		var entries = tree.entries();
-		var blobs = Object.create(null);
-		return Promise.map(entries, function (entry) {
-			return entry.getBlob()
-			.then(function getBlobContent(blob) {
-				return blob.content();
+		.then(function (tree) {
+			var entries = tree.entries();
+			var blobs = Object.create(null);
+			return Promise.map(entries, function (entry) {
+				return entry.getBlob()
+				.then(function getBlobContent(blob) {
+					return blob.content();
+				})
+				.then(function addBlobEntry(content) {
+					blobs[entry.sha()] = {
+						name: entry.path(),
+						content: content
+					};
+				});
 			})
-			.then(function addBlobEntry(content) {
-				blobs[entry.sha()] = {
-					name: entry.path(),
-					content: content
-				};
+			.then(function getBlobs() {
+				return blobs;
 			});
-		})
-		.then(function getBlobs() {
-			return blobs;
 		});
-	});
 };
 
 Gist.prototype.blobs = function blobs() {
-	var self = this;
 	return this._master()
-	.then(function getBlobsFromMaster(commit) {
-		return self._getBlobs(commit);
-	});
+		.then(this._getBlobs.bind(this));
 };
 
 module.exports = Gist;
--- app/api/routes/root.js
+++ app/api/routes/root.js
@@ -5,7 +5,7 @@ var Gist = require("../gist/index");
 module.exports = function route(request, response) {
 	var gist = new Gist(request.params.gistId);
 	gist.blobs()
-	.then(function (blobs) {
-		response(blobs);
-	});
+		.then(function (blobs) {
+			response(blobs);
+		});
 };
